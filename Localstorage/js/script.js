1348d1e4f351602baaee88f82900b74fd4cc7417
Michael Wasilewski <michal.wasilewski@stud.noroff.no>
2022-09-09 14:23:44 +0200

    learning about localStorage

---
+++ Localstorage/js/script.js
@@ -0,0 +1,29 @@
+// localStorage.setItem('firstName', 'Michael');
+
+// const firstName = LocalStorage.getItem('firstName');
+
+// localStorage.removeItem('firstName');
+// localStorage.clear();
+
+console.log("Ello");
+// How to set a local storage variable? 
+
+localStorage.setItem("name", "michael");
+
+// how to get local storage data/variable?
+
+// retrive data
+
+// getItem
+
+const theName = localStorage.getItem('name');
+console.log(theName);
+
+// remove local storage item// removeItem
+
+localStorage.removeItem('name');
+
+localStorage.setItem("name", "Michael");
+localStorage.setItem("age", 28);
+localStorage.setItem("city", "Oslo");
+// localStorage.clear();
