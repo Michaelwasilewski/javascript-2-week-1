97fd8c2e2b275d2ce63eadf7276b1dfc01c2c0b0
Michael Wasilewski <michal.wasilewski@stud.noroff.no>
2022-09-09 13:35:15 +0200

    converting JSON to JS and JS to JSON

---
+++ JSON/js/script.js
@@ -0,0 +1,35 @@
+// how to convert js to JSON
+
+
+const person = {
+    name: 'Ola Nordmann',
+    id: 9,
+    isAdmin: true,
+  };
+  
+const personJSON = JSON.stringify(person);
+  
+console.log(personJSON);
+// Logs:
+// {"name":"Ola Nordmann","id":9,"isAdmin":true}
+
+
+const userDetails = {
+  Name: "Michael",
+  Age: "28",
+  isBoss: true,
+};
+const userDetailsJSON = JSON.stringify(userDetails);
+console.log("userDetails JS Format",userDetailsJSON);
+  
+const userDataBackToJS = JSON.parse(userDetailsJSON);
+console.log(userDataBackToJS);
+
+// create a JSON data object and convert it to JS 
+
+
+const otherPersonJSON = '{ "name": "Ola Nordmann", "id": 9, "isAdmin": true }';
+
+const otherPerson = JSON.parse(otherPersonJSON);
+
+console.log(otherPerson)
